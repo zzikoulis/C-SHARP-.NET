91ce8e5e11ec8cde648e002e5fba6b97cd10c2e5
zzikoulis <zisis.zik@gmail.com>
2022-09-13 22:38:03 +0300

    Lesson2

---
+++ exercises/lesson2/Queue/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static int[] queue = new int[50];
+        static int last = -1;
+
+        static void Main(string[] args)
+        {
+            int? deQueueNum;
+
+            EnQueue(2);
+            EnQueue(4);
+            EnQueue(6);
+
+            PrintQueue();
+
+            if ((deQueueNum = DeQueue()) != null)
+            {
+                Console.WriteLine($"DeQueue of {deQueueNum} completed!");
+            }
+
+            PrintQueue();
+
+            if ((deQueueNum = DeQueue()) != null)
+            {
+                Console.WriteLine($"DeQueue of {deQueueNum} completed!");
+            }
+            if ((deQueueNum = DeQueue()) != null)
+            {
+                Console.WriteLine($"DeQueue of {deQueueNum} completed!");
+            }
+            if ((deQueueNum = DeQueue()) != null)
+            {
+                Console.WriteLine($"DeQueue of {deQueueNum} completed!");
+            }
+        }
+
+        public static void EnQueue(int num)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue is full! EnQueue operation can not be done.");
+            }
+            else
+            {
+                queue[++last] = num;
+                Console.WriteLine($"EnQueue of {num} completed!");
+            }
+        }
+
+        public static int? DeQueue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty! DeQueue operation can not be done.");
+                return null;
+            }
+            else
+            {
+                int num = queue[0];
+                for (int i = 0; i < last; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+                last--;
+                return num;
+            }
+        }
+
+        public static bool IsFull()
+        {
+            return (last == (queue.Length - 1));
+        }
+
+        public static bool IsEmpty()
+        {
+            return (last == -1);
+        }
+
+        public static void PrintQueue()
+        {
+            Console.WriteLine("Queue:");
+
+            for (int i = 0; i <= last; i++)
+            {
+                Console.WriteLine(queue[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ exercises/lesson2/Stack/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static int[] stack = new int[50];
+        static int top = -1;
+
+        static void Main(string[] args)
+        {
+            int? popNum;
+
+            Push(2);
+            Push(4);
+            Push(6);
+
+            PrintStack();
+           
+            if ((popNum = Pop()) != null)
+            {
+                Console.WriteLine($"Pop of {popNum} completed!");
+            }
+
+            PrintStack();
+
+            if ((popNum = Pop()) != null)
+            {
+                Console.WriteLine($"Pop of {popNum} completed!");
+            }
+            if ((popNum = Pop()) != null)
+            {
+                Console.WriteLine($"Pop of {popNum} completed!");
+            }
+            if ((popNum = Pop()) != null)
+            {
+                Console.WriteLine($"Pop of {popNum} completed!");
+            }
+        }
+
+        public static void Push(int num)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is full! Push operation can not be done.");
+            }
+            else
+            {
+                stack[++top] = num;
+                Console.WriteLine($"Push of {num} completed!");
+            }
+        }
+
+        public static int? Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty! Pop operation can not be done.");
+                return null;
+            }
+            else
+            {
+                return stack[top--];    
+            }
+        }
+
+        public static bool IsFull()
+        {
+            return (top == (stack.Length - 1));
+        }
+
+        public static bool IsEmpty()
+        {
+            return (top == -1);
+        }
+
+        public static void PrintStack()
+        {
+            Console.WriteLine("Stack:");
+
+            for (int i = top; i > -1; i--)
+            {
+                Console.WriteLine(stack[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
